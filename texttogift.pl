a1b4450b1fee83103a1d32e99ec1dfb221321cb9
Kenan <espadon@upf.pf>
2021-04-19 11:03:30 -1000

    Correction d'orthographe

--- texttogift.pl
+++ texttogift.pl
@@ -89,10 +89,10 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 		}
 		$le =~ m/\./;
 		$numq = NumQ($`);
-		print($fs "::Question_$numq :: $' \n\{ \n"); #on ajoute le titre de la question, l'énoncé puis l'ouverture des réponce
-		#on crée un tableau des réponces
+		print($fs "::Question_$numq :: $' \n\{ \n"); #on ajoute le titre de la question, l'énoncé puis l'ouverture des réponse
+		#on crée un tableau des réponses
 		my @t;
-		#on remplis ce tableau des réponce possible
+		#on remplis ce tableau des réponse possible
 		my $cpt = 0;
 		$le = <$fe>;
 		$ligne++;
@@ -104,19 +104,19 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 			$ligne++;
 			$cpt++;
 		}
-	#on conte le nombre de réponces proposer et de réponce juste
+	#on conte le nombre de réponses proposer et de réponse juste
 		my $reponce = @t;#affecter un tableau a une variable retourne sa taille
 		my $Breponce = split(/,/,$le);#le est la ligne des Answer en comptant le nombre de virgule+1 on obtion le nombre de reponce
 		$le =~ m/Answer:/i; #on retire Answer pour éviter les erreur avec le A
 		$le = $';
-		if($Breponce eq 1){ # cas pour 1 réponce juste bouton radio
+		if($Breponce eq 1){ # cas pour 1 réponse juste bouton radio
 			foreach (@t){
-				$_ =~ m/\./;#pour récupéré la lètre de la réponce
-				if($le =~ m/$`/){ #si les lêtre de la réponce est dans la ligne des réponce
+				$_ =~ m/\./;#pour récupéré la lètre de la réponse
+				if($le =~ m/$`/){ #si les lêtre de la réponse est dans la ligne des réponse
 					$_ =~ m/\./;
 					print($fs "=$' \n");
 				}
-				else{ #si les lêtre de la réponce n'est pas dans la ligne des réponce
+				else{ #si les lêtre de la réponse n'est pas dans la ligne des réponse
 					if($negatif ne ""){
 						$_ =~ m/\./;
 						print($fs "~$negatif$' \n");
@@ -127,12 +127,12 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 					}
 				}
 			}
-			print($fs "\}\n"); #on  oublie pas de fermer les réponces
+			print($fs "\}\n"); #on  oublie pas de fermer les réponses
 		}
 		else # dans les autres cas
 		{
-		my ($vj,$vf);#variable pour stocké les % de point par réponces juste ou fausse.
-			if($reponce-$Breponce eq 1 or $reponce-$Breponce eq 0){# si 1 réponce fausse
+		my ($vj,$vf);#variable pour stocké les % de point par réponses juste ou fausse.
+			if($reponce-$Breponce eq 1 or $reponce-$Breponce eq 0){# si 1 réponse fausse
 				$vj = 100/$Breponce;
 				$vf = $vj
 			}
@@ -143,16 +143,16 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 			
 			foreach (@t){
 				$_ =~ m/\./;
-				if($le =~ m/$`/){ #pour les réponce juste
+				if($le =~ m/$`/){ #pour les réponse juste
 					$_ =~ m/\./;
 					print($fs "~\%$vj\%$' \n");
 				}
-				else{ #pour les réponces fause
+				else{ #pour les réponses fause
 					$_ =~ m/\./;
 					print($fs "~\%-$vf\%$' \n");
 				}
 			}
-			print($fs "\}\n");#on  oublie pas de fermer les réponces
+			print($fs "\}\n");#on  oublie pas de fermer les réponses
 		}
 		
 	   }
@@ -167,15 +167,15 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 			$le =~ m/\./;
 			$numq = NumQ($`);
 			print($fs "::Question_$numq :: $'");# on indique le titre et la question
-			$le = <$fe>; # on vas sur la ligne de ma réponce
+			$le = <$fe>; # on vas sur la ligne de ma réponse
 			chomp $le;
 			$le =~ s/\r//g;
 			$ligne++;
 			$le =~ m/Answer: /i; # on retire Answer
-			print($fs "{#$'}\n"); #on met la réponce entre {}
+			print($fs "{#$'}\n"); #on met la réponse entre {}
 			$type = 3;
 	   }
-	   elsif( $le =~ m/shortanswer/i or ($le =~ m/\./ and $type == 4)){ #dans le cas d'une question réponce courte
+	   elsif( $le =~ m/shortanswer/i or ($le =~ m/\./ and $type == 4)){ #dans le cas d'une question réponse courte
 	   	$le = <$fe>; # on passe le type pour avoir le numérot de question et l'intitulé
 	   	$ligne++;
 	   	chomp $le;
@@ -183,18 +183,18 @@ if( -e "$ARGV[0]"){ #on verifie qu'un fichier a bien été passer en parametre
 	   	$le =~ m/\./;
 		$numq = NumQ($`);
 		print($fs "::Question_$numq :: $'");# on indique le titre et la question
-		$le = <$fe>;# on vas sur la ligne de ma réponce
+		$le = <$fe>;# on vas sur la ligne de ma réponse
 		chomp $le;
 		$le =~ s/\r//g;
 		$ligne++;
-		print($fs "\n{\n"); #on vas a la ligne et on ouvre les réponces
+		print($fs "\n{\n"); #on vas a la ligne et on ouvre les réponses
 		$le =~ m/Answer: /i;# on retire Answer
 		$le = $'; # vas avec la ligne du dessu
 		foreach(split(/, /,$le))#on crée un tableau pour caque valeur en utilisant la virgule comme séparateur puis on parcour le tableau
 		{
-			print($fs "=$_\n"); #on ajoute toute les réponces
+			print($fs "=$_\n"); #on ajoute toute les réponses
 		}
-		print($fs "}\n"); # on oublie pas de fermer les réponce et de retourner a la ligne
+		print($fs "}\n"); # on oublie pas de fermer les réponse et de retourner a la ligne
 		$type = 4;
 	   }
 	   elsif($le =~ m/Essay/i or ($le =~ m/\./ and $type == 5)){
